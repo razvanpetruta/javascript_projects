8e444cd8373bb8acfe7369cbb8ef7455323320ae
razvanpetruta <82545346+razvanpetruta@users.noreply.github.com>
2021-09-01 15:46:39 +0300

    Add files via upload

---
+++ dice_game/js/script.js
@@ -0,0 +1,19 @@
+function rollDice()
+{
+    // get a random number for the first dice
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+    // get a random number for the second dice
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    // choose the winner
+    if(randomNumber1 > randomNumber2)
+        document.querySelector("h1").innerHTML = "🥇 Player 1 Wins!";
+    else
+        if(randomNumber1 < randomNumber2)
+            document.querySelector("h1").innerHTML = "Player 2 Wins! 🥇";
+        else
+            document.querySelector("h1").innerHTML = "Draw!";
+}
